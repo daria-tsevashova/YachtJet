f4dcb0cf85fd03fde2005a867deefc488853a212
Daria Tsevashova <dariatsevashova@gmail.com>
2026-07-24 13:31:12 +0200

    Fix global styles in production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.module.css';
+import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
